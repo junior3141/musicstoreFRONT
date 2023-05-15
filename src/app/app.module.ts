d061b8e182a47b9951bc1ce38afcd9417d7c53a4
Jose Carlos <jcaranda00@gmail.com>
2023-05-15 12:49:05 +0200

    creacion del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductComponent } from './componente/products/product/product.component';
+import { CategoryComponent } from './componente/categories/category/category.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    CategoryComponent
   ],
   imports: [
     BrowserModule,
